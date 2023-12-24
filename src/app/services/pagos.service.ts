0bebdb76a9e8ff97037008a184f65a4d8b10544c
Edisonkko <120064527+Edisonkko@users.noreply.github.com>
2023-12-24 10:26:31 -0500

    Update pagos.service.ts

--- src/app/services/pagos.service.ts
+++ src/app/services/pagos.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PagosService {
-  URL_API: string = 'http://localhost/APIPAGOS/';
+  URL_API: string = 'https://dispensario.000webhostapp.com/APIPAGOS/';
 
   constructor(private http: HttpClient) { }
 
